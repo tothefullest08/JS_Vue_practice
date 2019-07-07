5a3d3b2e60a121c4a3f9c8d0700d4a9c5e812bfe
Harry Lee <tothefullest08@gmail.com>
2019-07-07 18:44:41 +0900

    190707 | JS | 인프런 강의 - 탭 구현

--- inflearn/baseTemplate/js/controllers/MainController.js
+++ inflearn/baseTemplate/js/controllers/MainController.js
@@ -1,5 +1,6 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../views/TabView.js'
 
 import SearchModel from '../models/SearchModel.js'
 
@@ -12,8 +13,17 @@ export default {
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e=> this.onResetForm())
+        
+        TabView.setup(document.querySelector('#tabs'))
+            .on('@change', e => this.onChangeTab(e.detail.tabName)
+                
+            )
+            
 
-        ResultView.setup(document.querySelector('#search-result'))  
+        ResultView.setup(document.querySelector('#search-result'))
+        
+        this.selctedTab = '추천 검색어'
+        this.renderView()
     },
 
     search(query) {
@@ -22,7 +32,12 @@ export default {
         SearchModel.list(query).then(data => {
             this.onSearchResult(data)
         })
+    },
 
+    renderView() {
+        console.log(tag, 'renderView()')
+        TabView.setActiveTab(this.selctedTab)
+        ResultView.hide()
     },
 
     onSubmit(input) {
@@ -38,4 +53,8 @@ export default {
     onSearchResult(data) {
         ResultView.render(data)
     },
+
+    onChangeTab(tabName) {
+        debugger
+    }
 }
---
+++ inflearn/baseTemplate/js/views/TabView.js
@@ -0,0 +1,32 @@
+import View from './View.js'
+
+const Tag = '[TabView]'
+
+const TabView = Object.create(View)
+
+TabView.setup = function(el) {
+    this.init(el)
+    this.bindClick()
+    return this
+}
+
+//active tab을 셋팅하는 함수
+TabView.setActiveTab = function(tabName){
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.className = li.innerHTML == tabName ? 'active' : ''
+    })
+}
+
+TabView.bindClick = function() {
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.addEventListener('click', e => this.onClick(li.innerHTML))
+    })
+}
+
+TabView.onClick = function (tabName) {
+    this.setActiveTab(tabName)
+    this.emit('@change', {tabName})
+}
+
+
+export default TabView
