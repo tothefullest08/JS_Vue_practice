d51ebf2f1a046bbc920813931f1420ec67b0aab1
Harry Lee <tothefullest08@gmail.com>
2019-07-09 22:21:24 +0900

    190709 | JS & VUE 인프런 강의 - Vue로 기능 구현하기

--- inflearn/base_vue/js/app.js
+++ inflearn/base_vue/js/app.js
@@ -1,14 +1,26 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
+
 
 new Vue({
     el: '#app',
     data: {
         query: '',
-        submitted: false, 
+        submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        keywords: [],
+        history: [],
         searchResult: [],
     },
+    created() {
+        this.selectedTab = this.tabs[0]
+        this.fetchKeyword()
+        this.fetchHistory()
+    },
     methods: {
-        onSubmit(e) {
+        onSubmit() {
             this.search()
         },
         onKeyup() {
@@ -22,11 +34,34 @@ new Vue({
                 this.submitted = true
                 this.searchResult = data
             })
+            HistoryModel.add(this.query)
+            this.fetchHistory()
         },
         resetForm() {
             this.query = ''
-            // todo remove result
+            this.submitted = false
+            this.searchResult = []
+        },
+        onClickTab(tab) {
+            this.selectedTab = tab
+        },
+        onClickKeyword(keyword){
+            this.query = keyword
+            this.search()
+        },
+        onClickRemoveHistory(keyword){
+            HistoryModel.remove(keyword)
+            this.fetchHistory()
+        },
+        fetchKeyword() {
+            KeywordModel.list().then(data => {
+                this.keywords = data
+            })
+        },
+        fetchHistory() {
+            HistoryModel.list().then(data => {
+                this.history = data
+            })
         }
-
     }
 })
