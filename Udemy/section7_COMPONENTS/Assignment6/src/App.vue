a0da6758afc3c37b07f65415f1bafb9bfc77debc
Harry Lee <tothefullest08@gmail.com>
2019-07-22 23:36:00 +0900

    udemy section 8

---
+++ Udemy/section7_COMPONENTS/Assignment6/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <Header></header>
+        <hr>
+        <div class="row">
+            <ServerList></ServerList>
+            <ServerDetail></ServerDetail>
+        </div>
+        <hr>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+
+import Header from './Header.vue'
+import ServerList from './ServerList.vue'
+import Footer from './Footer.vue'
+import ServerDetail from './ServerDetail.vue'
+
+export default {
+    components: {
+        'Header' : Header,
+        'ServerList': ServerList,
+        'Footer': Footer,
+        'ServerDetail': ServerDetail,
+    }
+}
+</script>
---
+++ Udemy/section7_COMPONENTS/start/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Components</h1>
+            </div>
+            <app-servers></app-servers>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
