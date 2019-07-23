41411493d084cb2c22edc3a719f3a869a3161365
Harry Lee <tothefullest08@gmail.com>
2019-07-23 23:33:27 +0900

    Udemy 강의 학습 - section 9, 10

---
+++ Udemy/section9_ADVANCED_COMPONENTS/Assignment8/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="content='appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="content='appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="content='appRed'">Load Red Template</button>
+                <hr>
+
+                {{content}}
+                <component :is="content"></component>
+                <app-blue>
+                    <div>파랑입니다</div>
+                </app-blue>
+                <app-green>
+                    <div>초록입니다</div>
+                </app-green>
+                <app-red>
+                    <div>빨강입니다</div>
+                </app-red>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data () {
+            return {
+                content:'appBlue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Udemy/section9_ADVANCED_COMPONENTS/start/src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+    </div>
+</template>
+<script>
+export default {
+    destroyed() {
+        console.log('Destroyed')
+    },
+    deactivated() {
+        console.log('Deactivated!')
+    },
+    activated() {
+        console.log('Activated')
+    },
+}
+</script>
+<style scoped>
+</style>
